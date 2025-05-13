2160ff5def7998c662b8934e0951b73451597393
Diogo Nóbrega <168549527+Diogo-Nobrega7@users.noreply.github.com>
2025-05-13 16:40:48 -0300

    Update Cliente.cpp

--- ContaBancariaSeparada/Cliente.cpp
+++ ContaBancariaSeparada/Cliente.cpp
@@ -1,10 +1,12 @@
 #include "Cliente.h"
 
+/// Criando a classe cliente
 Cliente::Cliente(string nome, string cpf) {
     this->nome = nome;
     this->cpf = cpf;
 }
 
+/// Metodos getters para acesasr os atributos privados
 string Cliente::getNome() const {
     return nome;
 }
